6b16de7634fbcf74213a71f701ac41c38097fce8
Mahiler <fernando.mahiler@gmail.com>
2020-02-27 23:59:56 -0500

    Actualizacion de nombres - AppInfo

--- app/screens/Account/AppInfo.js
+++ app/screens/Account/AppInfo.js
@@ -1,10 +1,13 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 export default function AppInfo() {
   return (
     <View>
-      <Text> Estamos en la Appnfo !!! </Text>
+      <Text> Fernando Chullo Mamani </Text>
+      <Text> Benjamin Andre Valdivia Navarrete </Text>
+      <Text> Gianella Naomi Alvarez </Text>
+      <Text> Catacora Llerena </Text>
     </View>
   );
 }
